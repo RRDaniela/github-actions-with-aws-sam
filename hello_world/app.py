27e35c6e0fc7d4503c1d355d5eafbf87303b0fbe
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2022-11-15 21:55:01 -0600

    Add html page

--- hello_world/app.py
+++ hello_world/app.py
@@ -34,10 +34,9 @@ def lambda_handler(event, context):
     #     raise e
 
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Segundo commmit",
-            "image":"https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg"
-            # "location": ip.text.replace("\n", "")
-        }),
+            "statusCode": 200,
+            "body": "<html><body><h1>Test</h1></body></html>",
+            "headers": {
+                'Content-Type': 'text/html',
+            }
     }
