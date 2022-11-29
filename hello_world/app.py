b54975717eca36239f59f502d113185c8a3a59ad
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2022-11-28 18:52:21 -0600

    Change image

--- hello_world/app.py
+++ hello_world/app.py
@@ -35,8 +35,9 @@ def lambda_handler(event, context):
 
     return {
             "statusCode": 200,
-            "body": "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><link rel=\"stylesheet\" href=\"style.css\" /><title>Proyecto Integrador</title></head><body><h1>Proyecto Integrador: Uso de GitHub Actions para implementar aplicaciones serverless</h1><p>En esta página se puede comprobar que se realizó el deployment del código.</p><img src=\"https://media.tenor.com/mhKBv0wFQQ0AAAAC/happy-dwight.gif\" alt=\"Happy Dwight\"></body></html>",
+            "body": "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><link rel=\"stylesheet\" href=\"style.css\" /><title>Proyecto Integrador</title></head><body><h1>Proyecto Integrador: Uso de GitHub Actions para implementar aplicaciones serverless</h1><p>En esta página se puede comprobar que se realizó el deployment del código.</p><img src=\"https://storage.googleapis.com/proudcity/elgl/uploads/2015/03/The-Office-Gifs-the-office-14820595-400-225.gif\" alt=\"Happy Dwight\"></body></html>",
             "headers": {
                 'Content-Type': 'text/html',
             }
     }
+
